e44cab0417e0a4088d5f06e0d6bccbcd915fc203
Divay Gupta <67044218+black-tul1p@users.noreply.github.com>
2023-03-11 17:21:59 -0500

    Refactored training code, added loss curve export

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import os
 import torch
+import torch.nn as nn
 from tqdm import tqdm
+import torch.optim as optim
 from datetime import datetime
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
@@ -9,11 +11,19 @@ from ScrapedDataset import ScrapedDataset
 import torchvision.transforms as transforms
 
 class Trainer:
-    def __init__(self, net, batch_size=32, shuffle=True):
-        self.net = net
+    def __init__(self, model, batch_size=32, shuffle=False, learning_rate=0.001):
+        self.model = model
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
         self.device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu") # type: ignore
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        self.train_loss = []
+        self.train_accuracy = []
+        self.test_loss = []
+        self.test_accuracy = []
 
-        # Define the dataset and data loaders
+         # Define the dataset and data loaders
         transform = transforms.Compose(
             [transforms.Resize((64, 64)),
              transforms.ToTensor(),
@@ -28,79 +38,90 @@ class Trainer:
 
         # Split the dataset into train and test sets
         train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
-
-        # Create the data loaders
-        self.trainloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
-        self.testloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-
-        # Initialize empty lists for train and test accuracy
-        self.train_accuracy = []
-        self.test_accuracy = []
+        self.train_data = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
+        self.test_data = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 
-    def train(self, criterion, optimizer, epochs=10):
-        # Set up the model and optimizer
-        self.net.to(self.device)
-
-        # Train the model
+    def train(self, epochs):
+        self.model.to(self.device)
         for epoch in tqdm(range(epochs), desc="Training", unit="epoch"):
             running_loss = 0.0
-            for i, data in enumerate(self.trainloader, 0):
+            correct = 0
+            total = 0
+            for data in self.train_data:
                 inputs, labels = data
                 inputs, labels = inputs.to(self.device), labels.to(self.device)
 
-                optimizer.zero_grad()
+                self.optimizer.zero_grad()
 
-                outputs = self.net(inputs)
-                loss = criterion(outputs, labels)
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
                 loss.backward()
-                optimizer.step()
+                self.optimizer.step()
 
                 running_loss += loss.item()
-                if i % 100 == 99:
-                    print('[%d, %5d] loss: %.3f' %
-                          (epoch + 1, i + 1, running_loss / 100))
-                    running_loss = 0.0
 
-            # Calculate train and test accuracy after each epoch
-            train_acc = self._accuracy(self.trainloader)
-            test_acc = self._accuracy(self.testloader)
-            self.train_accuracy.append(train_acc)
-            self.test_accuracy.append(test_acc)
-
-    def eval(self):
-        # Evaluate the model on the test set
-        accuracy = self._accuracy(self.testloader)
-        print('Accuracy of the network on the test images: %.2f%%' % accuracy)
-        return accuracy
-
-    def save_model(self):
-        torch.save(self.net.state_dict(), 'model.pth')
-        print('Model saved.')
-
-    def _accuracy(self, dataloader):
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for data in dataloader:
-                inputs, labels = data
-                inputs, labels = inputs.to(self.device), labels.to(self.device)
-                outputs = self.net(inputs)
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
-        accuracy = (100 * correct / total)
-        return accuracy
-    
+
+            train_loss = running_loss / len(self.train_data)
+            train_accuracy = 100 * correct / total
+            self.train_loss.append(train_loss)
+            self.train_accuracy.append(train_accuracy)
+
+            test_loss, test_accuracy = self.test()
+            self.test_loss.append(test_loss)
+            self.test_accuracy.append(test_accuracy)
+
+            # print('Epoch [%d], Train Loss: %.4f, Train Accuracy: %.2f%%, Test Loss: %.4f, Test Accuracy: %.2f%%'
+            #       % (epoch + 1, train_loss, train_accuracy, test_loss, test_accuracy))
+
+
+    def test(self):
+        self.model.eval()
+        test_loss = 0.0
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in self.test_data:
+                inputs, labels = data
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
+                test_loss += loss.item()
+
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        test_loss /= len(self.test_data)
+        test_accuracy = 100 * correct / total
+        return test_loss, test_accuracy
+
+
+    def plot_loss(self):
+        plt.plot(range(1, len(self.train_loss) + 1), self.train_loss, label='Train')
+        plt.plot(range(1, len(self.test_loss) + 1), self.test_loss, label='Test')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.title('Model Loss')
+        plt.legend()
+        folder_path = os.path.join('.', 'Plots')
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        plt.savefig(os.path.join(folder_path, f"loss_{datetime.now().strftime('%H_%M_%S')}"))
+
 
     def plot_accuracy(self):
-        plt.plot(range(1, len(self.train_accuracy) + 1), self.train_accuracy, label='Train')
-        plt.plot(range(1, len(self.test_accuracy) + 1), self.test_accuracy, label='Test')
+        plt.plot(self.train_accuracy, label='Train')
+        plt.plot(self.test_accuracy, label='Test')
         plt.xlabel('Epoch')
         plt.ylabel('Accuracy')
         plt.title('Model Accuracy')
+        plt.ylim(0, 110)
         plt.legend()
-        # plt.show()
         folder_path = os.path.join('.', 'Plots')
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
