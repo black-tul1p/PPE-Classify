0cfd5e050f4c5494f30acee495743ff50c56e2a3
Divay Gupta <67044218+black-tul1p@users.noreply.github.com>
2023-04-14 16:45:14 -0400

    Fix typo

--- Testing/runModel.py
+++ Testing/runModel.py
@@ -24,7 +24,7 @@ image_path = 'test.jpg'
 
 # Set transform
 transform = transforms.Compose(
-    [transforms.Resize((224, 244)),
+    [transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
 
